164e4399cc1600cd987e7353914873e71a76d349
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-07-17 12:40:18 -0600

    started final project for Programming with Functions class. Fetched information from own Google Sheet using the Sheety API, and mimic the response to not spend the limited API free calls

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+# OWN_GOOGLE_SHEET_API = 'https://api.sheety.co/6ada9ef7386080ce9ab42b293d2f77d5/flightDeals/prices'
+FAKE_API = 'data.json'
+
+def main():
+    # response = requests.get(OWN_GOOGLE_SHEET_API)
+    # response.raise_for_status()
+    # flights_data = response.json()
+
+    # print(flights_data)
+
+    file = open('data.json')
+    data = json.load(file)
+    print(data)
+
+main()
+
